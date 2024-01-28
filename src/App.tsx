12764da2749b41db5edc7d8d6859c829e56d6f18
Horikaze <adrianyt659@gmail.com>
2024-01-28 19:48:09 +0100

    updated key comp

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import { possiblePositions } from "./components/keysPositions";
 
 function App() {
   const [keysPos, setKeyPost] = useState(possiblePositions);
-
+  const [shuffelSpeed, setShuffelSpeed] = useState(220);
+  const [shuffelTimes, setShuffelTimes] = useState(10);
+  const [status, setStatus] = useState("-");
+  const [isCorrectVisible, setIsCorrectVisible] = useState(true);
   const shuffle = () => {
     setKeyPost((prev) => {
       // Create a copy of the array
@@ -21,28 +24,51 @@ function App() {
       return shuffledArray;
     });
   };
-  const shuffleNTimes = async (n: number) => {
-    for (let i = 0; i < n; i++) {
+  const shuffleNTimes = async () => {
+    setIsCorrectVisible(true);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    setIsCorrectVisible(false);
+
+    for (let i = 0; i < shuffelTimes; i++) {
       shuffle();
-      console.log("xddd");
-      await new Promise((resolve) => setTimeout(resolve, 250));
+      await new Promise((resolve) => setTimeout(resolve, shuffelSpeed));
     }
+  };
+  const handleKeyClick = (idx: number) => {
+    if (idx === 0) {
+      setStatus("WIN");
+      setIsCorrectVisible(true);
+      return;
+    }
+    setStatus("LOSE");
+    setIsCorrectVisible(true);
   };
   return (
     <>
       <div className="w-full h-full flex flex-col justify-center items-center">
-        <div className="h-[600px] w-[400px] bg-slate-500">
+        <div className="h-[600px] w-[400px] bg-slate-500 rounded-3xl">
           {keysPos.map((item, idx) => (
-            <Key key={idx} initialIndex={idx} y={item.y} x={item.x} />
+            <Key
+              key={idx}
+              initialIndex={idx}
+              y={item.y}
+              x={item.x}
+              keyClick={handleKeyClick}
+              isCorrectVisible={isCorrectVisible}
+            />
           ))}
         </div>
         <button
           onClick={() => {
-            shuffleNTimes(5);
+            shuffleNTimes();
           }}
+          className="border font-semibold py-2 px-3 m-2 rounded-lg bg-yellow-300 border-green-700
+          
+          "
         >
           shuffle
         </button>
+        <p className="text-3xl font-semibold text-yellow-300">{status}</p>
       </div>
     </>
   );
--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -3,16 +3,35 @@ type KeyProps = {
   initialIndex: number;
   x: number;
   y: number;
+  keyClick: (keyIdx: number) => void;
+  isCorrectVisible: boolean;
 };
-export default function Key({ x, y, initialIndex }: KeyProps) {
+export default function Key({
+  x,
+  y,
+  keyClick,
+  initialIndex,
+  isCorrectVisible,
+}: KeyProps) {
   return (
     <motion.div
       animate={{ x, y }}
-      className="w-[0px] h-[0px] bg-yellow-200"
+      className="w-[0px] h-[0px] bg-yellow-200 cursor-pointer group"
       transition={{ bounce: 1 }}
+      onClick={() => {
+        keyClick(initialIndex);
+      }}
     >
-      <div className="w-10 h-10 bg-sky-100 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
-        <p className="">{initialIndex + 1}</p>
+      <div
+        className={`w-12 h-12 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center ${
+          initialIndex === 0 && isCorrectVisible ? "keyShadow" : ""
+        }`}
+      >
+        <img
+          src="/KeyImage.png"
+          alt="key"
+          className="group-hover:scale-125 transition-all"
+        />
       </div>
     </motion.div>
   );
