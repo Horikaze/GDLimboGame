877a55918060855e1a581ad1fe9bdbfb55add435
Horikaze <adrianyt659@gmail.com>
2024-01-29 23:09:51 +0100

    added audio

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Key from "./components/Key";
 import { possiblePositions } from "./components/keysPositions";
 
 function App() {
   const [keysPos, setKeyPost] = useState(possiblePositions);
-  const [shuffelSpeed, setShuffelSpeed] = useState(220);
-  const [shuffelTimes, setShuffelTimes] = useState(10);
+  const [shuffelSpeed, setShuffelSpeed] = useState(300);
+  const [shuffelTimes, setShuffelTimes] = useState(31);
   const [status, setStatus] = useState("-");
   const [isCorrectVisible, setIsCorrectVisible] = useState(true);
+  const [isAudio, setIsAudio] = useState(true);
+  const audioRef = useRef<HTMLAudioElement>(null);
+  useEffect(() => {
+    audioRef.current!.volume = 0.1;
+  }, []);
   const shuffle = () => {
     setKeyPost((prev) => {
-      // Create a copy of the array
       const shuffledArray = [...prev];
-
-      // Perform the shuffle using a Fisher-Yates algorithm
       for (let i = shuffledArray.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [shuffledArray[i], shuffledArray[j]] = [
@@ -25,6 +27,10 @@ function App() {
     });
   };
   const shuffleNTimes = async () => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = 0;
+      audioRef.current.play();
+    }
     setStatus("-");
     setIsCorrectVisible(true);
     await new Promise((resolve) => setTimeout(resolve, 500));
@@ -46,8 +52,18 @@ function App() {
   };
   return (
     <>
+      <div
+        className="fixed flex justify-center px-2 py-1 items-center rounded-l-xl font-semibold top-0 right-0 cursor-pointer bg-yellow-400 z-30"
+        onClick={() => {
+          setIsAudio((prev) => !prev);
+        }}
+      >
+        Audio {isAudio ? "ON" : "OFF"}
+      </div>
+      <audio ref={audioRef} src={isAudio ? "limbo.mp3" : ""} />
+
       <div className="w-full h-full flex flex-col justify-center items-center">
-        <div className="h-[600px] w-[400px] bg-slate-500 rounded-3xl">
+        <div className="h-[600px] w-[400px] bg-slate-300">
           {keysPos.map((item, idx) => (
             <Key
               key={idx}
@@ -67,33 +83,9 @@ function App() {
           
           "
         >
-          shuffle
+          Start
         </button>
         <p className="text-3xl font-semibold text-yellow-300">{status}</p>
-        <div className="flex gap-x-2">
-          <div className="flex flex-col justify-center items-center text-xl font-semibold">
-            <p>Times</p>
-            <input
-              type="number"
-              className="rounded-xl px-1 py-2 w-24"
-              defaultValue={shuffelTimes}
-              onChange={(e) => {
-                setShuffelTimes(Number(e.target.value));
-              }}
-            />
-          </div>
-          <div className="flex flex-col justify-center items-center text-xl font-semibold">
-            <p>Speed</p>
-            <input
-              type="number"
-              className="rounded-xl px-1 py-2 w-24"
-              defaultValue={shuffelSpeed}
-              onChange={(e) => {
-                setShuffelSpeed(Number(e.target.value));
-              }}
-            />
-          </div>
-        </div>
       </div>
     </>
   );
